ac3a4570282c2568d18bf5a75c2d89bd7397705f
seonggeun an <eventscripts@nate.com>
2024-04-18 18:52:45 +0900

    [Fix]xcode file seperate

---
+++ Calculator 복사본/Calculator/Calculator/ContentView.swift
@@ -0,0 +1,120 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by mosi on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var setInput : String = ""
+    @State private var number1 : Double? = 0.0
+    @State private var number2 : Double? = 0.0
+    @State private var operators : String = ""
+    @State private var result : Double = 0.0
+    
+    let buttons : [[CalculatorButtons]] = [
+        [ .clear, .negative , .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
+    func Calculate() -> Double {
+        
+        switch(operators) {
+            case "+":
+            result = number1! + number2!
+            case "-":
+            result = number1! - number2!
+            case "X":
+            result = number1! * number2!
+            case "/":
+            result = number1! / number2!
+            case "%":
+            result = number1!.truncatingRemainder(dividingBy:number2!)
+            default:
+            break
+        }
+        return result
+    }
+    
+    var body: some View {
+        VStack{
+            Text(setInput)
+                .frame(maxWidth: 345, maxHeight: 50, alignment: .trailing)
+                .background(.gray)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            
+            ForEach(buttons, id:\.self) { row in
+                HStack {
+                    ForEach(row, id:\.self) { buttonchars in
+                        Button(action: {
+                            if buttonchars.rawValue == "AC" {
+                                setInput = ""
+                            }
+                            else if buttonchars.rawValue == "=" {
+                                number2 = Double(setInput)
+                                result = Calculate()
+                                setInput = String(format: "%.2f", result)
+                                number1 = 0.0
+                                number2 = 0.0
+                            }
+                            else if buttonchars.rawValue == "%" || buttonchars.rawValue == "/" || buttonchars.rawValue == "X" || buttonchars.rawValue == "-" || buttonchars.rawValue == "+"  {
+                                operators = buttonchars.rawValue
+                                number1 = Double(setInput)
+                                setInput = ""
+                                
+                            }
+                            else if buttonchars.rawValue == "+/-" {
+                                if setInput.prefix(1) != "-"{
+                                    setInput = "-"+setInput
+                                }
+                                else if setInput.prefix(1) == "-"{
+                                    setInput = setInput.replacingOccurrences(of: "-", with: "")
+                                }
+                            }
+                        
+                            else if !(setInput.isEmpty && (buttonchars.rawValue == "0" || buttonchars.rawValue == ".")) {
+                                setInput += buttonchars.rawValue
+                            }
+                            
+                        })
+                        {
+                            if "0" <= buttonchars.rawValue &&  buttonchars.rawValue <= "9" || buttonchars.rawValue == "." {
+                                Text(buttonchars.rawValue)
+                                    .frame(width: 80, height: 50)
+                                    .background(.gray)
+                                    .foregroundColor(.white)
+                            }
+                            else if buttonchars.rawValue == "AC" || buttonchars.rawValue == "+/-" || buttonchars.rawValue == "%" {
+                                Text(buttonchars.rawValue)
+                                    .frame(width: 80, height: 50)
+                                    .background(.black)
+                                    .foregroundColor(.white)
+                            }
+                            else {
+                                Text(buttonchars.rawValue)
+                                    .frame(width: 80, height: 50)
+                                    .background(.orange)
+                                    .foregroundColor(.white)
+                            }
+                            
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
