794b908090f1611a02c91d421a9def02a1808146
jihye <ganzi410@gmail.com>
2024-04-18 14:28:27 +0900

    make UI

--- Caculator/ContentView.swift
+++ Caculator/ContentView.swift
@@ -10,12 +10,129 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("12345")
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .background(.black)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            
+            HStack {
+                Text("AC")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                Text("+/-")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("%")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                Text("÷")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
+            HStack {
+                Text("7")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                Text("8")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                Text("9")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                Text("✕")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
+            HStack {
+                Text("4")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("5")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("6")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("-")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
+            HStack {
+                Text("1")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("2")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("3")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("+")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            HStack {
+                Text("0")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text(".")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.gray)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                Text("=")
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+            
         }
-        .padding()
+        
     }
 }
 
