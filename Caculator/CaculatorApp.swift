8b663bfb51e9c327424361d6f7fe499676dc200c
jihye <ganzi410@gmail.com>
2024-04-18 13:11:35 +0900

    Initial Commit

---
+++ Caculator/CaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaculatorApp.swift
+//  Caculator
+//
+//  Created by JIHYE SEOK on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
