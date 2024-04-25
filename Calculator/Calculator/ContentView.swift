3170de9fe44fedfa0e9fa3109da45756bfebe92d
YACHAE <104hsh76@gmail.com>
2024-04-25 15:34:49 +0900

    계산기완료

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,67 +8,99 @@
 import SwiftUI
 
 
+import SwiftUI
 
 struct ContentView: View {
-    @State private var totalNumber: String = "0"
-    private let buttonData: [[String]] = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "X"],
-        ["1", "2", "3", "+"],
-        ["0", "0", ",", "="]
-    ]
+    @State private var input: String = "0"
     
+    let buttons: [[String]] = [
+        ["AC", "+/-", "%", "/"],
+        ["7", "8", "9", "*"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+    ]
     
     var body: some View {
         ZStack {
-            Color.black.edgesIgnoringSafeArea(.all) // 배경화면 검은색
-            VStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 12) {
                 Spacer()
-                HStack {
-                    Spacer()
-                    Text(totalNumber)
-                        .padding()
-                        .font(.system(size: 73))
-                        .foregroundStyle(Color.white)
-                }
+                Text(input)
+                    .font(.system(size: 33)).bold()
+                    .padding()
+                    .foregroundColor(.white)
                 
-                HStack {
-                    Button {
-                        totalNumber = "0"
-                    } label: {
-                        Text("C").calculatorButtonStyle(backgroundColor: .gray)
-                        Text("/").calculatorButtonStyle(backgroundColor: .gray)
-                        Text("%").calculatorButtonStyle(backgroundColor: .gray)
-                        Text("$").calculatorButtonStyle(backgroundColor: .gray)
-                    }
-                }
-                
-                ForEach(buttonData, id: \.self) { line in
-                    HStack {
-                        ForEach(line, id: \.self) { button in
-                            Button {
-                                if totalNumber == "0" {
-                                    totalNumber = "7"
-                                } else {
-                                    totalNumber += "7"
-                                }
-                            } label: {
-                                Text(button).calculatorButtonStyle()
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Button(action: { buttonTapped(button) }) {
+                                Text(button)
+                                    .font(.title)
+                                    .frame(width: buttonWidth(button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                                    .background(Color.gray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(buttonWidth(button) / 2)
                             }
                         }
                     }
                 }
             }
+            .padding(.bottom)
         }
+    }
+    
+    func buttonTapped(_ button: String) {
+        switch button {
+        case "=":
+            calculateResult()
+        case "AC":
+            input = "0"
+        case "+/-":
+            if let inputValue = Double(input) {
+                input = String(inputValue * -1)
+            }
+        case "%":
+            if let inputValue = Double(input) {
+                input = String(inputValue / 100)
+            }
+        case ".":
+            if !input.contains(".") {
+                input += "."
+            }
+        default:
+            if input == "0" && button != "." {
+                input = button // '0' 다음에 숫자가 오면 그 숫자로 시작하도록 설정
+            } else {
+                input += button
+            }
+        }
+    }
+
+    
+    func calculateResult() {
+        let formattedString = input.replacingOccurrences(of: "/", with: "/").replacingOccurrences(of: "*", with: "*")
+        if let result = NSExpression(format: formattedString).expressionValue(with: nil, context: nil) as? NSNumber {
+            input = result.stringValue
+        } else {
+            input = "Error"
+        }
+    }
+    
+    func buttonWidth(_ button: String) -> CGFloat {
+        return button == "0" ? (UIScreen.main.bounds.width - 5 * 12) / 2 + 12 : (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
-
-
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
 
 
 #Preview {
     ContentView()
 }
+
