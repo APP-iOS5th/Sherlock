29dbcee3a397ddf1598db1723111c3a177260c50
YACHAE <104hsh76@gmail.com>
2024-04-28 22:20:57 +0900

    뼈대잡기

--- WordChainApp/WordChainApp/ContentView.swift
+++ WordChainApp/WordChainApp/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var currentWord = "apple"
+  @State private var userInput = ""
+  @State private var score = 0
   
   var body: some View {
 	VStack {
+	  Text(currentWord)
+	  TextField("Enter next word", text: $userInput)
 	  
+	  Button("submit") {
+		
+	  }
+	  
+	  Text("Score: \(score)")
 	}
   }
+  
+  
+  
 }
 
 #Preview {
