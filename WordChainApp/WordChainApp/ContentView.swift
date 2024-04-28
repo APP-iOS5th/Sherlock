b338a53755ddb56facc7ae27155a400c4968af33
YACHAE <104hsh76@gmail.com>
2024-04-28 22:45:15 +0900

    submit버튼을 누르면 점수와 다시하기를 눌러 게임 초기화

--- WordChainApp/WordChainApp/ContentView.swift
+++ WordChainApp/WordChainApp/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
   @State private var currentWord = "apple"
   @State private var userInput = ""
   @State private var score = 0
+  @State private var alertTitle = ""
+  @State private var showAlert = false
   
   var body: some View {
 	VStack {
@@ -18,14 +20,44 @@ struct ContentView: View {
 	  TextField("Enter next word", text: $userInput)
 	  
 	  Button("submit") {
-		
+		submitButton()
 	  }
 	  
 	  Text("Score: \(score)")
 	}
+	.alert(alertTitle, isPresented: $showAlert) {
+	  Button("다시하기") {
+		resetGame()
+	  }
+	} message: {
+	  Text ("당신의 점수는 \(score)점 입니다.")
+	}
   }
   
+  func submitButton() {
+	let word = userInput.lowercased()
+	
+	// 유효성 검사
+	fetchWordInfo(word: word) { isValid in
+	  if isValid && word.last == currentWord.first {
+		DispatchQueue.main.async {
+		  self.currentWord = word
+		  self.score += 1
+		  self.userInput = ""
+		}
+	  } else {
+		DispatchQueue.main.async {
+		  self.alertTitle = "Lose"
+		  self.showAlert = true
+		}
+	  }
+	}
+  }
   
+  func resetGame() {
+	currentWord = "apple"
+	score = 0
+  }
   
 }
 
