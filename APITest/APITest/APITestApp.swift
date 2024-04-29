e0786e046a414c8ca8d7a145547d91e9f87e9b9e
MadCow <lobasketve@naver.com>
2024-04-30 00:03:55 +0900

    word search

--- APITest/APITest/APITestApp.swift
+++ APITest/APITest/APITestApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct APITestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SearchWordView()
         }
     }
 }
---
+++ APITest/APITest/SearchWordView.swift
@@ -0,0 +1,88 @@
+//
+//  SearchWordView.swift
+//  APITest
+//
+//  Created by MadCow on 2024/4/29.
+//
+
+import SwiftUI
+
+struct SearchWordView: View {
+    @State private var findWord: String = ""
+    @State private var words: [String] = []
+    
+    var body: some View {
+        VStack {
+            TextField("search", text: $findWord)
+                .onChange(of: findWord) { _, newValue in
+                    if newValue.count > 0 {                    
+                        fetchWord(searchWord: newValue)
+                    } else {
+                        words = []
+                    }
+                }
+                .padding()
+            
+            List {
+                ForEach(words, id: \.self) { word in
+                    Text(word)
+                }
+            }
+            
+            Button("Test") {
+                fetchWord(searchWord: findWord)
+            }
+        }
+    }
+    
+    func fetchWord(searchWord: String) {
+        let session = URLSession.shared
+        
+        var str = "https://iapi.glosbe.com/iapi3/similar/similarPhrasesMany?p="
+        str += searchWord
+        str += "&l1=en&l2=ko&removeDuplicates=true&searchCriteria"
+        str += "=WORDLIST-ALPHABETICALLY-2-s%3BPREFIX-PRIORITY-2-s%3BTRANSLITERATED"
+        str += "-PRIORITY-2-s%3BFUZZY-PRIORITY-2-s%3BWORDLIST-ALPHABETICALLY-2"
+        str += "-r%3BPREFIX-PRIORITY-2-r%3BTRANSLITERATED-PRIORITY"
+        str += "-2-r%3BFUZZY-PRIORITY-2-r&env=ko"
+        
+        guard let url = URL(string: str) else { return }
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("error > \(error)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("No Result!!")
+                return
+            }
+            
+            guard let data = data else {
+                print("No Result Data")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let phrases = json["phrases"] as? [[String: Any]] {
+                    var wordList: [String] = []
+                    for phrase in phrases {
+                        if let word = phrase["phrase"] as? String {
+                            wordList.append(word)
+                        }
+                    }
+                    words = wordList
+                }
+            } catch {
+                print("error > \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+}
+
+#Preview {
+    SearchWordView()
+}
